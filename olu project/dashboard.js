16d8032c07f1fc95285b2bb0cf9b3a42b060d7b5
BlessedPeace5 <akunnapeace48@gmail.com>
2024-07-26 12:12:55 +0100

    another

--- olu project/dashboard.js
+++ olu project/dashboard.js
@@ -1,15 +1,30 @@
-function search(){
-const userinput=document.getElementById("search-input").ariaValueMax.toLowerCase();
+    localStorage.setItem("searchResult", JSON.stringify({
+        "schools for girls":"forgirls.html",
+        "schools for girls":"forboys.html",
+        "schools for the disabled":"fordisabled.html",
+        "schools for deaf":"fordeaf.html"
+    }));
+    function search(){
+        const userinput = document.getElementById("search-input").value.toLowerCase();
+        const searchResult =JSON.parse(localStorage.getItem("searchResult"));
 
-if(userinput === "schools for girls"){
-    window.location.href ="forgirls.html";
-}else if ( userinput === "Schools for boys"){
-    window.location.href="forboys.html";
-}else if (userinput=== "Schools for the disabled"){
-    window.location.href= "fordisabled.html";
-}else if (userinput=== "Schools for deaf"){
-    window.location.href= "fordeaf.html";
-}else{
-    alert("Not found");
-}
-}
+        if(searchReasult[userinput]){        
+            window.location.href= searchResult[userinput];
+        } else {
+            alert("not found");
+        }
+    }
+    
+    // if(userinput === "schools for girls"){
+    //     window.location.href ="forgirls.html";
+    // }else if ( userinput === "Schools for boys"){
+    //     window.location.href="forboys.html";
+    // }else if (userinput=== "Schools for the disabled"){
+    //     window.location.href= "fordisabled.html";
+    // }else if (userinput=== "Schools for deaf"){
+    //     window.location.href= "fordeaf.html";
+    // }else{
+    //     alert("Not found");
+    // }
+    // }
+    
